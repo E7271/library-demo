ee404a05210ccb0533e8c3822279660072f3c746
andy <nguyen>
2019-04-27 16:10:39 -0700

    Add file IO

--- src/Library.java
+++ src/Library.java
@@ -1,4 +1,6 @@
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Holds books
@@ -39,16 +41,16 @@ public class Library {
 
         Library output_library = new Library("TempName");
 
-        for (int x = 0; x < lib1.catalog.size(); x++){
+        for (int x = 0; x < lib1.catalog.size(); x++) {
             temp_array.add(lib1.catalog.get(x));
         }
-        for (int x = 0; x < lib2.catalog.size(); x++){
+        for (int x = 0; x < lib2.catalog.size(); x++) {
             temp_array.add(lib2.catalog.get(x));
         }
 
-        for(int x = 0; x < temp_array.size(); x++){
-            for(int y = 0 ; y < temp_array.size(); y++){
-                if(temp_array.get(x).getTitle().equals(temp_array.get(y).getTitle())){
+        for (int x = 0; x < temp_array.size(); x++) {
+            for (int y = 0; y < temp_array.size(); y++) {
+                if (temp_array.get(x).getTitle().equals(temp_array.get(y).getTitle())) {
                     temp_array.get(x).setCopies(temp_array.get(y).getCopies() + temp_array.get(x).getCopies());
                 }
             }
@@ -62,13 +64,55 @@ public class Library {
         return catalog.size();
     }
 
-    public static void print(Library input){
+    public static void print(Library input) {
         System.out.println(input.getLibrary_name() + " catalog: " + input.get_catalog_size() + " entries");
-        for(int x = 0; x < input.catalog.size(); x++){
-            System.out.println(input.catalog.get(x).getTitle() + ", " + input.catalog.get(x).getCopies());
+        for (int x = 0; x < input.catalog.size(); x++) {
+            System.out.println("  - " + input.catalog.get(x).getTitle() + ", " + input.catalog.get(x).getCopies());
 
         }
 
     }
 
+    public void save_library(String filename) {
+
+        try (PrintWriter pw = new PrintWriter(new FileWriter(filename))) {
+
+            pw.println(get_catalog_size());
+
+            for (Book b : catalog) {
+                pw.println(b.getAuthor_first());
+                pw.println(b.getAuthor_last());
+                pw.println(b.getTitle());
+                pw.println(b.getCopies());
+                pw.println(b.getPublication_date());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error saving library " + library_name + " to file " + filename);
+            e.printStackTrace();
+        }
+    }
+
+    public void load_library(String filename) {
+
+        try (Scanner scanner = new Scanner(new File(filename))) {
+            int books_count = scanner.nextInt();
+            scanner.nextLine();
+
+            while (books_count --> 0) {
+                String first = scanner.nextLine();
+                String last = scanner.nextLine();
+                String title = scanner.nextLine();
+                int copies = scanner.nextInt();
+                scanner.nextLine();
+                String publication = scanner.nextLine();
+
+                Book b = new Book(first, last, title, copies, publication);
+                this.add(b);
+            }
+
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/TestDriver.java
+++ src/TestDriver.java
@@ -1,12 +1,40 @@
 public class TestDriver {
 
     public static void main(String args[]) {
+
         Library forest_grove = new Library("Forest Grove City Library");
+        forest_grove.load_library("forest_grove.txt");
+        Library.print(forest_grove);
+        System.out.println();
+
+        Library beaverton = new Library("Beaverton City Library");
+        beaverton.load_library("beaverton.txt");
+        Library.print(beaverton);
+        System.out.println();
+
+        Library tualatin = new Library("Tualatin Public Library");
+        tualatin.load_library("tualatin.txt");
+        Library.print(tualatin);
+        System.out.println();
+
+        Library tempLib = Library.merge(tualatin, forest_grove);
+        Library wccls = Library.merge(tempLib,beaverton);
+        wccls.setLibrary_name("WCCLS");
+        Library.print(wccls);
+    }
+
+
+}
+
+/*
+        Library forest_grove = new Library("Forest Grove City Library");
+
         forest_grove.add(new Book("Arthur", "Miller", "Death of a Salesman", 4, "1949"));
         forest_grove.add(new Book("Nicholas", "Sparks", "The Notebook", 2, "1996"));
         forest_grove.add(new Book("Barbara", "Kingsolver", "The Poisonwood Bible", 2, "1999"));
         forest_grove.add(new Book("Greg", "Mortenson", "Three Cups of Tea", 3, "2006"));
 
+        forest_grove.save_library("forest_grove.txt");
         //Library.print(forest_grove);
 
 
@@ -17,6 +45,7 @@ public class TestDriver {
         beaverton.add(new Book("Nicholas", "Sparks", "The Notebook", 8, "1996"));
         beaverton.add(new Book("George", "Orwell", "Nineteen Eighty-Four", 14, "1949"));
         beaverton.add(new Book("Grace", "Linn", "Where the Mountain Meets the Moon", 9, "2009"));
+        beaverton.save_library("beaverton.txt");
 
         Library tualatin = new Library("Tualatin Public Library");
         tualatin.add(new Book("Harper", "Lee", "To Kill a Mockingbird", 5, "1960"));
@@ -24,14 +53,6 @@ public class TestDriver {
         tualatin.add(new Book("Brian", "Jacques", "Mossflower", 4, "1988"));
         tualatin.add(new Book("Barbara", "Kingsolver", "The Poisonwood Bible", 7, "1999"));
         tualatin.add(new Book("Paulo", "Coelho", "The Alchemist", 5, "1988"));
+        tualatin.save_library("tualatin.txt");
 
-
-        Library tempLib = Library.merge(tualatin, forest_grove);
-        Library wccls = Library.merge(tempLib,beaverton);
-
-        Library.print(wccls);
-
-    }
-
-
-}
+        */
